0ef8c118fa8f06c10c26fac3157003417a38524d
Eugene <z.pilipenko123@gmail.com>
2020-12-25 12:36:39 +0300

    add all files 3

--- Individual2(2).py
+++ Individual2(2).py
@@ -6,7 +6,6 @@
 # вычисляющий корень произвольной степени, и метод для вычисления числа PI в данной
 # степени.
 
-
 import math
 
 
@@ -14,23 +13,32 @@ class Number:
     def __init__(self, num=0):
         self.num = float(num)
 
-    def truediv(self, other):
-        return Number(self.num / other.num)
+    def mul(self, other):
+        return Number(self.num * other.num)
 
     def sub(self, other):
         return Number(self.num - other.num)
 
 
 class Real(Number):
-    def super(Real, self).__init__(num):
-        self.num = float(num)
+    def __init__(self, num=0):
+        super(Real, self).__init__(num)
 
     def sqrtn(self, n):
         return self.num ** (1 / n)
 
+    def pow(self, other):
+        return math.pi ** other.num
+
 
 if __name__ == '__main__':
-    r1 = Number(3)
-    r2 = Real(5)
-
-    print(r1.truediv(r1/r2))
+    r1 = Number(2)
+    r2 = Real(4)
+    r = r1.mul(r2)
+    print(r.num)
+    r = r1.sub(r2)
+    print(r.num)
+    r = r2.sqrtn(2)
+    print(r)
+    r = r2.pow(r1)
+    print(r)
